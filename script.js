0b49bf910ae2a1fdf951de7f317f15b929483cf2
daviddiaz <daviddiazstoica05@gmail.com>
2026-06-09 16:14:17 +0200

    juego base terminado

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
-let snake = [
-    { x: 200, y: 200 }, // Index 0 : The head
-    { x: 180, y: 200 }, // Index 1: A piece of the body 
-    { x: 160, y: 200 },  // Index 2: The tail 
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
 
+const speedMilliS = 100;
+
+const scoreElement = document.getElementById('score');
+const highScoreElement = document.getElementById('high-score');
+
+let widthScreen =  window.innerWidth; 
+let heightScreen = window.innerHeight;
+
+let responsiveBoard = Math.min( widthScreen, heightScreen);
+
+responsiveBoard = responsiveBoard * 0.8 
+
+canvas.width = responsiveBoard; 
+canvas.height= responsiveBoard;
+
+const boxSize = Math.floor(responsiveBoard/20) ;
+
+let snake = [
+    { x: boxSize * 10 , y: boxSize * 10 }, // Index 0 : The head
+    { x: boxSize * 9, y: boxSize * 10 }, // Index 1: A piece of the body 
+    { x: boxSize * 8, y: boxSize * 10 },  // Index 2: The tail 
 ];
-let food = { x: 100, y: 100 };
+let food = { x: boxSize * 15, y: boxSize * 15 };
 let direction = 'RIGHT';
 let gameInterval; 
 let isGameOver = false;
 let score = 0;
 let highScore = 0; 
 
-
-const canvas = document.getElementById('board');
-const ctx = canvas.getContext('2d');
-
-const speedMilliS = 100;
-const boxSize = 20;
-
-const scoreElement = document.getElementById('score');
-const highScoreElement = document.getElementById('high-score');
-
-
 function resetVar() {
 
     snake = [
-    { x: 200, y: 200 }, // Index 0 : The head
-    { x: 180, y: 200 }, // Index 1: A piece of the body 
-    { x: 160, y: 200 },  // Index 2: The tail 
-
+        { x: boxSize * 10 , y: boxSize * 10 }, 
+        { x: boxSize * 9, y: boxSize * 10 }, 
+        { x: boxSize * 8, y: boxSize * 10 },  
     ];
+    food = { x: boxSize * 15, y: boxSize * 15 };
 
     direction = 'RIGHT';
     isGameOver = false;
     score = 0;
     scoreElement.textContent = score;
-
-    
 }
-
 
 //Paint the board 
 function drawBoard(){
 
     ctx.fillStyle = '#9bbc0f';      
-    ctx.fillRect(0,0,400,400);
+    ctx.fillRect(0,0,canvas.width,canvas.height);
 
     ctx.fillStyle = 'red';
     ctx.fillRect(food.x, food.y, boxSize,boxSize); 
@@ -57,9 +62,9 @@ function drawBoard(){
     if (isGameOver) {
         
         ctx.font = " bold 30px Arial"
-        ctx.fillStyle = 'black'
+        ctx.fillStyle = 'red'
         ctx.textAlign = "center"
-        ctx.fillText("GAME OVER", 200, 200);
+        ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
 
     }
 
@@ -74,27 +79,30 @@ addEventListener('keydown', function(event) {
         direction = 'LEFT';
 
     } else if (event.key === 'ArrowRight' && direction != 'LEFT') {
+        
         console.log('Right arrow pressed');
         direction = 'RIGHT';
 
     } else if (event.key === 'ArrowUp' && direction != 'DOWN') {
+        
+        event.preventDefault();
         console.log('Up arrow pressed');
         direction =  'UP';
 
     } else if (event.key === 'ArrowDown' && direction != 'UP') {
+
+        event.preventDefault();
         console.log('Down arrow pressed');
         direction = 'DOWN';
+    
     } else if (event.key === 'Enter' && isGameOver ||
         event.key === ' ' && isGameOver){
-        
-        event.preventDefault;
-
+        event.preventDefault();
         resetVar();
         gameInterval = setInterval(gameLoop,speedMilliS);
     } 
 
 });
-
 
 //Logic of the game 
 function moveSnake(){
@@ -148,22 +156,17 @@ function moveSnake(){
 
 };
 
-
-
 function gameLoop() {
    
     moveSnake(); 
     gameOver();
     drawBoard(); 
 
-
-
 }
 
 /* Works like a metronome, repeating the function every 100 milliseconds. 
 As opposed to setTimeout() which only executes once*/
 gameInterval = setInterval(gameLoop, speedMilliS);
-
 
 function foodRandomGenerator(){
 
@@ -187,29 +190,25 @@ function foodRandomGenerator(){
 
 function gameOver() {
     
-
-
-    if (snake[0].x < 0 || snake[0].x >= 400 ||
-        snake[0].y < 0|| snake[0].y >= 400) {
+    if (snake[0].x < 0 || snake[0].x >= canvas.width ||
+        snake[0].y < 0|| snake[0].y >= canvas.height) {
         
         console.log("hit the wall")
         
        clearInterval(gameInterval);
 
        if (score > highScore) {
-        
+
+            console.log("I pass the filter")        
             highScore = score; 
             highScoreElement.textContent = highScore;
 
        }
         isGameOver = true;
 
-
     }
 
-
     snake.slice(1).forEach(body => {
-
 
         if(snake[0].x === body.x  && snake[0].y === body.y){
 
@@ -217,10 +216,8 @@ function gameOver() {
 
             clearInterval(gameInterval);
 
-
             isGameOver = true;
 
         } 
     });
 }
-
